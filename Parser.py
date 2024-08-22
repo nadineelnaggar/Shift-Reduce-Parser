de67246f51ed09c77acde47e9c8b279b039588e1
nadineelnaggar <nadine_el_naggar@hotmail.com>
2024-08-22 15:38:47 +0400

    try to use the same data as shift reduce to implement cyk parser, and then commenting it out when it didnt work

--- Parser.py
+++ Parser.py
@@ -3,6 +3,8 @@ from nltk.parse.shiftreduce import ShiftReduceParser
 # from nltk import grammar, parse
 from nltk import CFG, parse
 
+from nltk.ccg.chart import CCGChartParser
+
 grammar = CFG.fromstring(
     """
     S -> NP VP
@@ -27,3 +29,13 @@ parser = parse.ShiftReduceParser(grammar, trace=2)
 # print(parser)
 for p in parser.parse(sent):
         print(p)
+
+
+# parser2 = CCGChartParser(grammar, trace=2)
+#
+# parser2.parse(sent)
+#
+# for p in parser2.parse(sent):
+#     print(p)
+
+
